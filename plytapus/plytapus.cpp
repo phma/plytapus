7990e357096556f7432192b0f0563497f7994640
Pierre Abbat <phma@bezitopo.org>
2020-05-24 19:54:44 +0000

    Add double version of ldecimal function

--- plytapus/plytapus.cpp
+++ plytapus/plytapus.cpp
@@ -2,9 +2,107 @@
 
 #include <fstream>
 #include <string>
+#include <cfloat>
 
 namespace plytapus
 {
+
+std::string ldecimal(double x,double toler)
+{
+  double x2;
+  int h,i,iexp,chexp;
+  size_t zpos;
+  char *dotpos,*epos,*pLcNumeric;
+  std::string ret,s,m,antissa,exponent,saveLcNumeric;
+  char buffer[32],fmt[8];
+  assert(toler>=0);
+  pLcNumeric=setlocale(LC_NUMERIC,nullptr);
+  if (pLcNumeric)
+    saveLcNumeric=pLcNumeric;
+  setlocale(LC_NUMERIC,"C");
+  if (toler>0 && x!=0)
+  {
+    iexp=floor(log10(fabs(x/toler))-1);
+    if (iexp<0)
+      iexp=0;
+  }
+  else
+    iexp=DBL_DIG-1;
+  h=-1;
+  i=iexp;
+  while (true)
+  {
+    sprintf(fmt,"%%.%de",i);
+    sprintf(buffer,fmt,x);
+    x2=atof(buffer);
+    if (h>0 && (fabs(x-x2)<=toler || i>=DBL_DIG+3))
+      break;
+    // GCC atof("inf")==0. MSVC atof("inf")=INFINITY.
+    if (fabs(x-x2)>toler || std::isnan(x-x2) || i<=0)
+      h=1;
+    i+=h;
+  }
+  dotpos=strchr(buffer,'.');
+  epos=strchr(buffer,'e');
+  if (epos && !dotpos) // e.g. 2e+00 becomes 2.e+00
+  {
+    memmove(epos+1,epos,buffer+31-epos);
+    dotpos=epos++;
+    *dotpos='.';
+  }
+  if (dotpos && epos)
+  {
+    m=std::string(buffer,dotpos-buffer);
+    antissa=std::string(dotpos+1,epos-dotpos-1);
+    exponent=std::string(epos+1);
+    if (m.length()>1)
+    {
+      s=m.substr(0,1);
+      m.erase(0,1);
+    }
+    iexp=atoi(exponent.c_str());
+    zpos=antissa.find_last_not_of('0');
+    antissa.erase(zpos+1);
+    iexp=stoi(exponent);
+    if (iexp<0 && iexp>-5)
+    {
+      antissa=m+antissa;
+      m="";
+      iexp++;
+    }
+    if (iexp>0)
+    {
+      chexp=iexp;
+      if (chexp>antissa.length())
+	chexp=antissa.length();
+      m+=antissa.substr(0,chexp);
+      antissa.erase(0,chexp);
+      iexp-=chexp;
+    }
+    while (iexp>-5 && iexp<0 && m.length()==0)
+    {
+      antissa="0"+antissa;
+      iexp++;
+    }
+    while (iexp<3 && iexp>0 && antissa.length()==0)
+    {
+      m+='0';
+      iexp--;
+    }
+    sprintf(buffer,"%d",iexp);
+    exponent=buffer;
+    ret=s+m;
+    if (antissa.length())
+      ret+='.'+antissa;
+    if (iexp)
+      ret+='e'+exponent;
+  }
+  else
+    ret=buffer;
+  setlocale(LC_NUMERIC,saveLcNumeric.c_str());
+  return ret;
+}
+
 File::File(const PATH_STRING& filename)
 	: m_filename(filename),
 	m_parser(std::make_unique<FileParser>(filename))
